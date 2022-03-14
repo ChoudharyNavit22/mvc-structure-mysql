ce1b3d48946c80d099e878cd5824c9667354e109
ChoudharyNavit22 <navitchoudhary22@gmail.com>
2022-03-14 23:35:00 +1100

    Initial Commit

---
+++ models/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Navit
+ */
+ 'use strict'
+ const Users = require('./users')
+ async function init () {
+   await Users.sync();
+ };
+ 
+ init();
+ module.exports = {
+   Users
+ };
